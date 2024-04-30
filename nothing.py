708ef820c296d26c0d88a53abaf56f83fcf5da51
Martin Yeo <martin.yeo@metoffice.gov.uk>
2024-04-30 18:28:10 +0100

    Mature implementation.

--- nothing.py
+++ nothing.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+"""Functionality to support do-nothing workflows.
+
+https://blog.danslimmon.com/2019/07/15/do-nothing-scripting-the-key-to-gradual-automation/
+"""
+
+import abc
+from argparse import ArgumentParser
+from collections.abc import Callable
 from dataclasses import dataclass
 from datetime import datetime
 import json
@@ -8,30 +17,61 @@ from typing import Any, Self
 
 
 @dataclass
-class Progress:
+class Progress(abc.ABC):
+    """The base class for do-nothing workflows.
+
+    Includes functionality for saving and loading progress. Intended entry
+    point is :meth:`main`, which provides a command-line interface. Ensure
+    all variables that track progress are included as attributes, and are
+    serialisable to JSON; attributes beginning with `_` are excluded from the
+    save/load functionality.
+    """
+
     latest_complete_step: int = -1
+    """:meth:`run` will begin at this step + 1. Enables resuming from a saved state."""
+
+    _dry_run: bool = False
+    """If True, will only attempt instantiation, without saving or running steps."""
+
+    def __new__(cls, *args: list[Any], **kwargs: dict[str, Any]) -> "Progress":
+        # It is essential to correct operation that cls itself is @dataclass
+        #  decorated, not just inheriting decoration from a parent class.
+        #  (Otherwise cls attributes will not be included in cls.__init__).
+        cls = dataclass(cls)
+        return super().__new__(cls)
+
+    def __setattr__(self, key: Any, value: Any) -> None:
+        # Call :meth:`save` whenever an attribute is set.
+        super().__setattr__(key, value)
+        if not self._dry_run and self.ready:
+            self.save()
 
     def __post_init__(self) -> None:
-        nothing_dir = Path().cwd() / ".nothing"
-        if nothing_dir.exists() and not nothing_dir.is_dir():
-            message = f"{nothing_dir} exists but is not a directory."
-            raise RuntimeError(message)
-        elif not nothing_dir.exists():
-            nothing_dir.mkdir()
+        """Set up logging and save-file, then call :meth:`run`."""
+        if self._dry_run:
+            return
 
-        file_stem = nothing_dir / datetime.now().strftime("%Y%m%d-%H%M%S")
+        file_stem = self._get_file_stem()
+        self._logger = self._get_logger(file_stem)
+        self._file_path = file_stem.with_suffix(".json")
+        self._logger.info(f"Progress will be saved to: {self._file_path}")
 
-        self._logger = logging.getLogger(f"nothing-{file_stem.name}")
-        self._logger.setLevel(logging.DEBUG)
+        self.save()
+        self.run()
+
+    @staticmethod
+    def _get_logger(file_stem: Path) -> logging.Logger:
+        """Create a logger for informing the user and recording activity."""
+        logger = logging.getLogger(f"nothing-{file_stem.name}")
+        logger.setLevel(logging.DEBUG)
         file = logging.FileHandler(
             filename=file_stem.with_suffix(".log"),
             mode="w",
         )
         file.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
         file.setLevel(logging.DEBUG)
-        self._logger.addHandler(file)
-        # `console` allows a configurable level at which logging is also sent
-        #  to STDOUT.
+        logger.addHandler(file)
+        # `console` allows a configurable level at which logging is also sent to STDOUT.
         console = logging.StreamHandler(stdout)
         console.setFormatter(
             logging.Formatter(
@@ -39,26 +79,176 @@ class Progress:
             )
         )
         console.setLevel(logging.INFO)
-        self._logger.addHandler(console)
+        logger.addHandler(console)
 
-        self._file_path = file_stem.with_suffix(".json")
-
-        self.save()
+        return logger
 
     @classmethod
-    def load(cls, file_path: Path) -> Self:
+    def _get_file_stem(cls) -> Path:
+        """Create a date-stamped path for storing progress and logs.
+
+        Creates the parent directory if necessary.
+        """
+        nothing_dir = Path().cwd() / ".nothing"
+        if nothing_dir.exists() and not nothing_dir.is_dir():
+            message = f"{nothing_dir} exists but is not a directory."
+            raise RuntimeError(message)
+        elif not nothing_dir.exists():
+            nothing_dir.mkdir()
+
+        date_time_string = datetime.now().strftime("%Y%m%d-%H%M%S")
+        return nothing_dir / f"{cls.__name__}_{date_time_string}"
+
+    @classmethod
+    def load(cls, file_path: Path, dry_run: bool = False) -> Self:
+        """Instantiate by loading a previous state from a saved file."""
         kwargs = json.loads(file_path.read_text())
-        return cls(**kwargs)
+        return cls(_dry_run=dry_run, **kwargs)
+
+    @classmethod
+    def main(cls) -> None:
+        """Command-line interface for the do-nothing workflow."""
+        parser = ArgumentParser(
+            description=cls.cmd_description(),
+        )
+        subparsers = parser.add_subparsers(required=True)
+
+        helps = {
+            "new": "Start the workflow fresh from step 0.",
+            "load": (
+                "Resume a previous workflow by loading its saved JSON progress file. "
+                "NOTE this file can be manually edited to start at an alternative step "
+                "or use different values."
+            ),
+            "template": (
+                "Create a new JSON progress file, which can be loaded by the `load` "
+                "subcommand. The file is populated with the default values; intended "
+                "for manual editing."
+            ),
+        }
+
+        new, load, template = [
+            subparsers.add_parser(name=name, help=helps[name], description=helps[name])
+            for name in helps
+        ]
+
+        load.add_argument(
+            "file_path",
+            type=Path,
+            help="The path of the JSON progress file to be loaded.",
+        )
+
+        def create_template_file() -> None:
+            instance = cls(_dry_run=True)
+            instance._logger = logging.getLogger("nothing")
+            instance._file_path = cls._get_file_stem().with_name(
+                f"{cls.__name__}_template.json"
+            )
+            instance.save()
+            print(f"Template saved to: {instance._file_path}")
+
+        new.set_defaults(func=lambda _: cls())
+        load.set_defaults(func=lambda p: cls.load(p.file_path))
+        template.set_defaults(func=lambda _: create_template_file())
+
+        parsed = parser.parse_args()
+        parsed.func(parsed)
+
+    @property
+    def ready(self) -> bool:
+        """Return True once the essential attributes have been set."""
+        return all(
+            [
+                k in self.__dict__
+                for k in (
+                    "_logger",
+                    "_file_path",
+                )
+            ]
+        )
+
+    @property
+    def state(self) -> dict[str, Any]:
+        """Current values of all non-private dataclass attributes.
+
+        Intended that these are the values that are used through the do-nothing
+        workflow and track its progress. They are the values that are required
+        for recreating the instance e.g. via :meth:`load`.
+        """
+        return {
+            k: self.__dict__[k] for k in self.__match_args__ if not k.startswith("_")
+        }
+
+    def run(self) -> None:
+        """Iteratively run the functions in :meth:`steps`.
+
+        Begins at :attr:`latest_complete_step` + 1, which enables started from
+        previously saved progress via :meth:`load`.
+        """
+
+        index_first = self.latest_complete_step + 1
+        for step in self.steps()[index_first:]:
+            index_current = self.latest_complete_step + 1
+            self._logger.info(f"*** STEP {index_current} STARTING ***")
+            step(self=self)
+            self._logger.info(f"*** STEP {index_current} COMPLETE ***")
+            self.latest_complete_step = index_current
+        self._logger.info("*** WORKFLOW COMPLETE ***")
 
     def save(self) -> None:
-        state = self.__dict__.copy()
-        _file_path = state.pop("_file_path", None)
-        _logger = state.pop("_logger", None)
-        if _file_path and _logger:
-            json_rep = json.dumps(state, indent=0)
-            Path(self._file_path).write_text(json_rep)
-            self._logger.info(f"Progress saved to {self._file_path}")
+        """Save :attr:`state` to a JSON file, enabling later reloading.
 
-    def __setattr__(self, key: Any, value: Any) -> None:
-        super().__setattr__(key, value)
-        self.save()
+        Includes validation that the saved file can be successfully reloaded.
+        """
+        self._logger.debug(f"Saving state: {self.state}")
+        json_rep = json.dumps(self.state, indent=0)
+        Path(self._file_path).write_text(json_rep)
+        self._logger.debug("Save complete.")
+
+        try:
+            _ = self.__class__.load(self._file_path, dry_run=True)
+        except Exception as exception:
+            message = (
+                f"{self.__class__.__name__} instance saves to an "
+                f"unloadable file - exception below:\n\n{exception}"
+            )
+            self._logger.error(message)
+            raise ValueError(message)
+
+    @classmethod
+    @abc.abstractmethod
+    def cmd_description(cls) -> str:
+        """Return a string describing the command-line interface."""
+        return "SciTools do-nothing workflow"
+
+    @classmethod
+    @abc.abstractmethod
+    def steps(cls) -> list[Callable[..., None]]:
+        """Return a list of functions that represent the steps of the process."""
+        return NotImplemented
+
+
+class Demo(Progress):
+    var_1: int = 0
+    var_2: str | None = None
+
+    def set_var_1(self) -> None:
+        self.var_1 = datetime.now().day
+
+    def set_var_2(self) -> None:
+        self.var_2 = input("Input a string: ")
+
+    @classmethod
+    def cmd_description(cls) -> str:
+        return "Demo workflow for nothing.py"
+
+    @classmethod
+    def steps(cls) -> list[Callable[..., None]]:
+        return [
+            cls.set_var_1,
+            cls.set_var_2,
+        ]
+
+
+if __name__ == "__main__":
+    Demo.main()
